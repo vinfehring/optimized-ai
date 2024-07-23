a7ff5b52f240b80c06163aa4b154a5f7695216ed
Vincenzo Fehring <vinfehring@gmail.com>
2024-07-23 17:28:44 -0500

    Initialized clerk authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,10 @@ import {
   SheetTrigger
 } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
+import {
+  ClerkProvider
+} from "@clerk/nextjs";
+
 import "./globals.css";
 
 const fontSans = FontSans({
@@ -28,70 +32,72 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        <div className="flex min-h-screen w-full flex-col">
-          <div className="flex flex-col min-h-dvh">
-            <header className="flex items-center h-35 px-4 border-b shrink-0 md:px-6">
-              <Link href="#" className="flex items-center gap-2 text-lg font-semibold" prefetch={true}>
-                <Image src="/images/logo-color.webp" width={381} height={80} alt="Optimized AI" />
-                <span className="sr-only">Optimized AI</span>
-              </Link>
-              <nav className="flex-col hidden gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 ml-auto">
-                <Link href="/" className="font-bold" prefetch={true}>
-                  Home
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <div className="flex min-h-screen w-full flex-col">
+            <div className="flex flex-col min-h-dvh">
+              <header className="flex items-center h-35 px-4 border-b shrink-0 md:px-6">
+                <Link href="#" className="flex items-center gap-2 text-lg font-semibold" prefetch={true}>
+                  <Image src="/images/logo-color.webp" width={381} height={80} alt="Optimized AI" />
+                  <span className="sr-only">Optimized AI</span>
                 </Link>
-                <Link href="/features" className="text-muted-foreground" prefetch={true}>
-                  Features
-                </Link>
-                <Link href="/pricing" className="text-muted-foreground" prefetch={true}>
-                  Pricing
-                </Link>
-                <Button>Sign In</Button>
-              </nav>
-              <Sheet>
-                <SheetTrigger asChild>
-                  <Button
-                    variant="outline"
-                    size="icon"
-                    className="shrink-0 md:hidden"
-                  >
-                    <Menu className="h-5 w-5" />
-                    <span className="sr-only">Toggle navigation menu</span>
-                  </Button>
-                </SheetTrigger>
-                <SheetContent side="left">
-                  <nav className="grid gap-6 text-lg font-medium">
-                    <Link
-                      href="/"
-                      className="flex items-center gap-2 text-lg font-semibold"
+                <nav className="flex-col hidden gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 ml-auto">
+                  <Link href="/" className="font-bold" prefetch={true}>
+                    Home
+                  </Link>
+                  <Link href="/features" className="text-muted-foreground" prefetch={true}>
+                    Features
+                  </Link>
+                  <Link href="/pricing" className="text-muted-foreground" prefetch={true}>
+                    Pricing
+                  </Link>
+                  <Button>Sign In</Button>
+                </nav>
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <Button
+                      variant="outline"
+                      size="icon"
+                      className="shrink-0 md:hidden"
                     >
-                      <Image src="/images/logo-color.webp" width={381} height={80} alt="Optimized AI" />
-                    </Link>
-                    <Link href="/" className="font-bold" prefetch={true}>
-                      Home
-                    </Link>
-                    <Link href="/features" className="text-muted-foreground" prefetch={true}>
-                      Features
-                    </Link>
-                    <Link href="/pricing" className="text-muted-foreground" prefetch={true}>
-                      Pricing
-                    </Link>
-                    <Button>Sign Up</Button>
-                    <Button>Sign In</Button>
-                  </nav>
-                </SheetContent>
-              </Sheet>
-            </header>
-            {children}
+                      <Menu className="h-5 w-5" />
+                      <span className="sr-only">Toggle navigation menu</span>
+                    </Button>
+                  </SheetTrigger>
+                  <SheetContent side="left">
+                    <nav className="grid gap-6 text-lg font-medium">
+                      <Link
+                        href="/"
+                        className="flex items-center gap-2 text-lg font-semibold"
+                      >
+                        <Image src="/images/logo-color.webp" width={381} height={80} alt="Optimized AI" />
+                      </Link>
+                      <Link href="/" className="font-bold" prefetch={true}>
+                        Home
+                      </Link>
+                      <Link href="/features" className="text-muted-foreground" prefetch={true}>
+                        Features
+                      </Link>
+                      <Link href="/pricing" className="text-muted-foreground" prefetch={true}>
+                        Pricing
+                      </Link>
+                      <Button>Sign Up</Button>
+                      <Button>Sign In</Button>
+                    </nav>
+                  </SheetContent>
+                </Sheet>
+              </header>
+              {children}
+            </div>
           </div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
