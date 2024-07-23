6c84f52785aa7bf7e00ce224dd71acaf3266aa8f
Vincenzo Fehring <vinfehring@gmail.com>
2024-07-23 13:12:19 -0500

    Added API route stubs

---
+++ src/app/api/promotions/route.ts
@@ -0,0 +1,27 @@
+export async function GET() {
+
+}
+
+export async function POST() {
+  
+}
+
+export async function PUT() {
+  
+}
+
+export async function PATCH() {
+  
+}
+
+export async function DELETE() {
+  
+}
+
+export async function HEAD() {
+  
+}
+
+export async function OPTIONS() {
+  
+}
