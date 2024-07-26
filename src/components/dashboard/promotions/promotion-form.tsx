7d0e76b3140ba751a7a0b70b27bf52a132224b26
Vincenzo Fehring <vinfehring@gmail.com>
2024-07-26 15:28:51 -0500

    Created new promotion form #OPTIM-16

---
+++ src/components/dashboard/promotions/promotion-form.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  promotion_name: z.string(),
+  promotion_details: z.string()
+});
+
+export function PromotionForm() {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      promotion_name: "",
+      promotion_details: ""
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="promotion_name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Promotion Name</FormLabel>
+              <FormControl>
+                <Input placeholder="BOGO Mondays" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="promotion_details"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Promotion Details</FormLabel>
+              <FormControl>
+                <Input placeholder="Buy one burger, get one free!" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
