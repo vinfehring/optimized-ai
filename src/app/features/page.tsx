41a7a12d34a47a67d4198b9c01d924ef0b838dd0
Vincenzo Fehring <vinfehring@gmail.com>
2024-07-26 10:58:29 -0500

    Fixed misspelling #OPTIM-54

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -6,7 +6,7 @@ export default function Features() {
           <div className="container space-y-12 px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Unlock Your Loytal Customer&apos;s Potential</h2>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Unlock Your Loyal Customer&apos;s Potential</h2>
                 <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   Our platform provides a suite of powerful features to help you expand, engage with, and
                   deliver exceptional results to your loyalty program users.
